6f265b239fe7c542b288d244186ce4c56a1a8c1d
Crescencs <crescencio.ps.code@gmail.com>
2022-10-13 09:01:26 -0500

    purge junk files

--- src/main/java/pasteleria/ecommerce/repositories/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package pasteleria.ecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pasteleria.ecommerce.entities.User;
-
-@Repository
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
