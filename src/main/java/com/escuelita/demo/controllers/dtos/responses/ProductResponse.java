01d280ab558d144808ade238466f71590a2e00cf
Crescencs <crescencio.ps.code@gmail.com>
2022-11-09 08:46:47 -0600

    simple upload files

--- src/main/java/com/escuelita/demo/controllers/dtos/responses/ProductResponse.java
+++ src/main/java/com/escuelita/demo/controllers/dtos/responses/ProductResponse.java
@@ -3,7 +3,8 @@ package com.escuelita.demo.controllers.dtos.responses;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class ProductResponse {
 
     private Long id;
@@ -11,4 +12,5 @@ public class ProductResponse {
     private Double price;
     private String description;
     private String quantity;
+    private String cakePicture;
 }
---
+++ src/main/java/com/escuelita/demo/services/interfaces/IFileService.java
@@ -0,0 +1,7 @@
+package com.escuelita.demo.services.interfaces;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IFileService {
+    String upload(MultipartFile multipartFile);
+}
