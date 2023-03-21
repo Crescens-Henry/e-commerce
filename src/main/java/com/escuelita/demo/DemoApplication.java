810d9abf81ea8e49ec6df089071734a94a9a9e30
Crescens-Henry <crescencio.ps.code@gmail.com>
2023-03-21 08:42:18 -0600

    updates imports

--- src/main/java/com/escuelita/demo/DemoApplication.java
+++ src/main/java/com/escuelita/demo/DemoApplication.java
@@ -19,8 +19,8 @@ public class DemoApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://127.0.0.1:5173/")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedOrigins("*")
+						.allowedMethods("*")
 						.allowedHeaders("*")
 						.exposedHeaders("*");
 			}
